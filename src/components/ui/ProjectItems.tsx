ffeb21166d38febf02c08a9ff9e104d12ffbad63
Kritika Singh <kritikasingh00.com@gmail.com>
2025-05-20 03:32:31 +0530

    updates

--- src/components/ui/ProjectItems.tsx
+++ src/components/ui/ProjectItems.tsx
@@ -1,102 +1,107 @@
-import React from 'react'
-import star from "../../app/assets/starbg.png"
-import Image from 'next/image'
-import kaizenyou from "../../app/assets/projects/kaizenyou.png"
-import vaccitime from "../../app/assets/projects/vaccitime.png"
-import codefiesta from "../../app/assets/projects/codefiesta.png"
-import hiresum from "../../app/assets/projects/hiresum.png"
-import chatbot from "../../app/assets/projects/chatbot.png"
-import litfest from "../../app/assets/projects/litfest.png"
+import React from "react";
+import star from "../../app/assets/starbg.png";
+import Image from "next/image";
+import kaizenyou from "../../app/assets/projects/kaizenyou.png";
+import vaccitime from "../../app/assets/projects/vaccitime.png";
+import codefiesta from "../../app/assets/projects/codefiesta.png";
+import hiresum from "../../app/assets/projects/hiresum.png";
+import chatbot from "../../app/assets/projects/chatbot.png";
+import litfest from "../../app/assets/projects/litfest.png";
 
 import { BentoGrid, BentoGridItem } from "../ui/bento-grid";
 
-
 const ProjectItems = () => {
-    return (
-        <div className='w-full min-h-screen relative bg-black pb-20'>
-            <Image
-                src={star}
-                alt='star'
-                className='w-full absolute bottom-0 z-0 h-full animate-pulse'
-            />
-            <BentoGrid className="max-w-4xl mx-auto">
-                {items.map((item, i) => (
-                    <BentoGridItem
-                        key={i}
-                        title={item.title}
-                        description={item.description}
-                        header={item.header}
-                        tags={item.tags}
-                        headerUrl={item.headerUrl}
-                        url={item.url}
-                        className={i === 3 || i === 6 ? "md:col-span-2" : ""}
-                    />
-                ))}
-            </BentoGrid>
-        </div>
-    )
-}
+  return (
+    <div className="w-full min-h-screen relative bg-black pb-20">
+      <Image
+        src={star}
+        alt="star"
+        className="w-full absolute bottom-0 z-0 h-full animate-pulse"
+      />
+      <BentoGrid className="max-w-4xl mx-auto">
+        {items.map((item, i) => (
+          <BentoGridItem
+            key={i}
+            title={item.title}
+            description={item.description}
+            header={item.header}
+            tags={item.tags}
+            headerUrl={item.headerUrl}
+            url={item.url}
+            className={i === 3 || i === 6 ? "md:col-span-2" : ""}
+          />
+        ))}
+      </BentoGrid>
+    </div>
+  );
+};
 
-export default ProjectItems
-
-
+export default ProjectItems;
 
 const items = [
-
-    {
-        title: "KaizenYou Website",
-        description: "The official website for a edtech startup KaizenYou.",
-        header: kaizenyou,
-        tags: ["NextJS", "TailwindCSS", "GSAP"],
-        url: "https://www.kaizenyou.in/",
-        headerUrl: "https://github.com/Kritikasingh2004/kaizenyou.git",
-    },
-    {
-        title: "Litfest Website",
-        description: "The official website for the literature event of Lucknow.",
-        header: litfest,
-        tags: ["VueJS", "PicoCSS", "Firebase"],
-        url: "https://enlit.club/",
-        headerUrl: "https://github.com/Kritikasingh2004/litfest.git",
-    },
-    {
-        title: "CodeFiesta Website",
-        description: "Website made for a tech fest at Lucknow University.",
-        header: codefiesta,
-        tags: ["VueJS", "Bootstrap"],
-        url: "https://codefiesta-chi.vercel.app/",
-        headerUrl: "https://github.com/Kritikasingh2004/codefiesta.git",
-    },
-    {
-        title: "VacciTime",
-        description: "A website to remind you about your child's vaccination schedule.",
-        header: vaccitime,
-        tags: ["Firebase", "NextJS", "TailwindCSS"],
-        url: "https://vacci-time.vercel.app/",
-        headerUrl: "https://github.com/Kritikasingh2004/Vacci-Time.git",
-    },
-    {
-        title: "ChatBot Frontend",
-        description: "FrontEnd for a image based chatbot.",
-        header: chatbot,
-        tags: ["NextJS", "TailwindCSS", "Aceternity UI"],
-        url: "https://chatbot-app-livid.vercel.app/",
-        headerUrl: "https://github.com/Kritikasingh2004/chatbot-app.git",
-    },
-    {
-        title: "HireSum FrontEnd",
-        description: "FrontEnd of a website for hiring candidates.",
-        header: hiresum,
-        tags: ["ReactJS", "CSS"],
-        url: "https://kritikasingh2004.github.io/hiresum/",
-        headerUrl: "https://github.com/Kritikasingh2004/hiresum.git",
-    },
-    {
-        title: "Coding Soon...",
-        description: "Let me cook...For more project see my github profile.",
-        tags: ["Project"],
-        url: "",
-        headerUrl: "",
-    },
-
+  {
+    title: "Campus/mart",
+    description:
+      "Built a platform allowing students to buy/sell used items securely.",
+    header: kaizenyou,
+    tags: ["NextJS", "TailwindCSS", "Appwrite"],
+    url: "https://campusmart-two.vercel.app/",
+    headerUrl: "https://github.com/Kritikasingh2004/campusmart.git",
+  },
+  {
+    title: "KaizenYou Website",
+    description: "The official website for a edtech startup KaizenYou.",
+    header: kaizenyou,
+    tags: ["NextJS", "TailwindCSS", "GSAP"],
+    url: "https://www.kaizenyou.in/",
+    headerUrl: "https://github.com/Kritikasingh2004/kaizenyou.git",
+  },
+  {
+    title: "Litfest Website",
+    description: "The official website for the literature event of Lucknow.",
+    header: litfest,
+    tags: ["VueJS", "PicoCSS", "Firebase"],
+    url: "https://enlit.club/",
+    headerUrl: "https://github.com/Kritikasingh2004/litfest.git",
+  },
+  {
+    title: "CodeFiesta Website",
+    description: "Website made for a tech fest at Lucknow University.",
+    header: codefiesta,
+    tags: ["VueJS", "Bootstrap"],
+    url: "https://codefiesta-chi.vercel.app/",
+    headerUrl: "https://github.com/Kritikasingh2004/codefiesta.git",
+  },
+  {
+    title: "VacciTime",
+    description:
+      "A website to remind you about your child's vaccination schedule.",
+    header: vaccitime,
+    tags: ["Firebase", "NextJS", "TailwindCSS"],
+    url: "https://vacci-time.vercel.app/",
+    headerUrl: "https://github.com/Kritikasingh2004/Vacci-Time.git",
+  },
+  {
+    title: "ChatBot Frontend",
+    description: "FrontEnd for a image based chatbot.",
+    header: chatbot,
+    tags: ["NextJS", "TailwindCSS", "Aceternity UI"],
+    url: "https://chatbot-app-livid.vercel.app/",
+    headerUrl: "https://github.com/Kritikasingh2004/chatbot-app.git",
+  },
+  {
+    title: "HireSum FrontEnd",
+    description: "FrontEnd of a website for hiring candidates.",
+    header: hiresum,
+    tags: ["ReactJS", "CSS"],
+    url: "https://kritikasingh2004.github.io/hiresum/",
+    headerUrl: "https://github.com/Kritikasingh2004/hiresum.git",
+  },
+  {
+    title: "Coding Soon...",
+    description: "Let me cook...For more project see my github profile.",
+    tags: ["Project"],
+    url: "",
+    headerUrl: "",
+  },
 ];
--- src/components/ui/SkillPreview.tsx
+++ src/components/ui/SkillPreview.tsx
@@ -2,175 +2,163 @@ import React from "react";
 import { useId } from "react";
 
 export function SkillPreview() {
-    return (
-        <div className="py-10">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 md:gap-2 max-w-7xl mx-auto">
-                {grid.map((feature) => (
-                    <div
-                        key={feature.title}
-                        className="relative bg-gradient-to-b from-neutral-900 to-neutral-950 p-6 rounded-3xl overflow-hidden hover:shadow-[0_0_10px_rgba(253,224,71,1)] hover:border-yellow-300 transition-shadow duration-300 cursor-pointer"
-                    >
-                        <Grid size={20} />
-                        <p className="text-base font-bold text-yellow-300 relative z-20">
-                            {feature.title}
-                        </p>
-                        <p className="text-neutral-400 mt-4 text-base font-normal relative z-20">
-                            {feature.description}
-                        </p>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="py-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 md:gap-2 max-w-7xl mx-auto">
+        {grid.map((feature) => (
+          <div
+            key={feature.title}
+            className="relative bg-gradient-to-b from-neutral-900 to-neutral-950 p-6 rounded-3xl overflow-hidden hover:shadow-[0_0_10px_rgba(253,224,71,1)] hover:border-yellow-300 transition-shadow duration-300 cursor-pointer"
+          >
+            <Grid size={20} />
+            <p className="text-base font-bold text-yellow-300 relative z-20">
+              {feature.title}
+            </p>
+            <p className="text-neutral-400 mt-4 text-base font-normal relative z-20">
+              {feature.description}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 const grid = [
-    {
-        title: "NextJs",
-        description:
-            "I have made some projects on NextJs just like this portfolio.",
-    },
-    {
-        title: "ReactJs",
-        description:
-            "Since you have to know ReactJs to work with NextJs, I know ReactJs too.",
-    },
-    {
-        title: "TailwindCSS",
-        description:
-            "I have used TailwindCSS in this portfolio and I literally love it.",
-    },
-    {
-        title: "Firebase",
-        description:
-            "I have used Firebase for authentication and database.",
-    },
-    {
-        title: "Appwrite",
-        description:
-            "I am still a beginner in this but I am learning it.",
-    },
-    {
-        title: "Bootstrap",
-        description:
-            "I have used Bootstrap in some of my older projects.",
-    },
-    {
-        title: "GitHub",
-        description:
-            "Who doesn't know GitHub? I have used it in all of my projects.",
-    },
-    {
-        title: "Python",
-        description:
-            "I am doing dsa in python.",
-    },
-    {
-        title: "VueJs",
-        description:
-            "I have worked on a few projects based on VueJs.",
-    },
-    {
-        title: "Figma",
-        description:
-            "I know how to use it.",
-    },
-    {
-        title: "Html/ Css",
-        description:
-            "These are the basics of web development.",
-    },
-    {
-        title: "Javascript",
-        description:
-            "I have a nice understanding of Javascript.",
-    },
+  {
+    title: "NextJs",
+    description:
+      "I have made some projects on NextJs just like this portfolio.",
+  },
+  {
+    title: "NestJs",
+    description: "I am working on NestJs during my internship as an SDE.",
+  },
+  {
+    title: "Typescript",
+    description: "I have used Typescript in this portfolio and I love it.",
+  },
+  {
+    title: "TailwindCSS",
+    description:
+      "I have used TailwindCSS in this portfolio and I literally love it.",
+  },
+  {
+    title: "ReactJs",
+    description:
+      "Since you have to know ReactJs to work with NextJs, I know ReactJs too.",
+  },
+  {
+    title: "Appwrite",
+    description: "I have used Appwrite for authentication and database.",
+  },
+  {
+    title: "Firebase",
+    description: "I have used Firebase for authentication and database.",
+  },
+  {
+    title: "Bootstrap",
+    description: "I have used Bootstrap in some of my older projects.",
+  },
+  {
+    title: "GitHub",
+    description:
+      "Who doesn't know GitHub? I have used it in all of my projects.",
+  },
+  {
+    title: "VueJs",
+    description: "I have worked on a few projects based on VueJs.",
+  },
+  {
+    title: "Html/ Css",
+    description: "These are the basics of web development.",
+  },
 ];
 
 interface GridProps {
-    pattern?: number[][];
-    size?: number;
+  pattern?: number[][];
+  size?: number;
 }
 
 export const Grid: React.FC<GridProps> = ({ pattern, size }) => {
-    const p = pattern ?? [
-        [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
-        [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
-        [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
-        [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
-        [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
-    ];
+  const p = pattern ?? [
+    [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
+    [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
+    [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
+    [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
+    [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
+  ];
 
-    return (
-        <div className="pointer-events-none absolute left-1/2 top-0  -ml-20 -mt-2 h-full w-full [mask-image:linear-gradient(white,transparent)]">
-            <div className="absolute inset-0 bg-gradient-to-r  [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] from-zinc-900/30 to-zinc-900/30 opacity-100">
-                <GridPattern
-                    width={size ?? 20}
-                    height={size ?? 20}
-                    x="-12"
-                    y="4"
-                    squares={p}
-                    className="absolute inset-0 h-full w-full  mix-blend-overlay fill-white/10 stroke-white/10"
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div className="pointer-events-none absolute left-1/2 top-0  -ml-20 -mt-2 h-full w-full [mask-image:linear-gradient(white,transparent)]">
+      <div className="absolute inset-0 bg-gradient-to-r  [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] from-zinc-900/30 to-zinc-900/30 opacity-100">
+        <GridPattern
+          width={size ?? 20}
+          height={size ?? 20}
+          x="-12"
+          y="4"
+          squares={p}
+          className="absolute inset-0 h-full w-full  mix-blend-overlay fill-white/10 stroke-white/10"
+        />
+      </div>
+    </div>
+  );
 };
 
 interface GridPatternProps {
-    width: number;
-    height: number;
-    x: string;
-    y: string;
-    squares: number[][];
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    [key: string]: any; // For any other props
+  width: number;
+  height: number;
+  x: string;
+  y: string;
+  squares: number[][];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [key: string]: any; // For any other props
 }
 
 export const GridPattern: React.FC<GridPatternProps> = ({
-    width,
-    height,
-    x,
-    y,
-    squares,
-    ...props
+  width,
+  height,
+  x,
+  y,
+  squares,
+  ...props
 }) => {
-    const patternId = useId();
+  const patternId = useId();
 
-    return (
-        <svg aria-hidden="true" {...props}>
-            <defs>
-                <pattern
-                    id={patternId}
-                    width={width}
-                    height={height}
-                    patternUnits="userSpaceOnUse"
-                    x={x}
-                    y={y}
-                >
-                    <path d={`M.5 ${height}V.5H${width}`} fill="none" />
-                </pattern>
-            </defs>
+  return (
+    <svg aria-hidden="true" {...props}>
+      <defs>
+        <pattern
+          id={patternId}
+          width={width}
+          height={height}
+          patternUnits="userSpaceOnUse"
+          x={x}
+          y={y}
+        >
+          <path d={`M.5 ${height}V.5H${width}`} fill="none" />
+        </pattern>
+      </defs>
+      <rect
+        width="100%"
+        height="100%"
+        strokeWidth={0}
+        fill={`url(#${patternId})`}
+      />
+      {squares && (
+        <svg x={x} y={y} className="overflow-visible">
+          {squares.map(([x, y]: number[]) => (
             <rect
-                width="100%"
-                height="100%"
-                strokeWidth={0}
-                fill={`url(#${patternId})`}
+              strokeWidth="0"
+              key={`${x}-${y}`}
+              width={width + 1}
+              height={height + 1}
+              x={x * width}
+              y={y * height}
             />
-            {squares && (
-                <svg x={x} y={y} className="overflow-visible">
-                    {squares.map(([x, y]: number[]) => (
-                        <rect
-                            strokeWidth="0"
-                            key={`${x}-${y}`}
-                            width={width + 1}
-                            height={height + 1}
-                            x={x * width}
-                            y={y * height}
-                        />
-                    ))}
-                </svg>
-            )}
+          ))}
         </svg>
-    );
+      )}
+    </svg>
+  );
 };
